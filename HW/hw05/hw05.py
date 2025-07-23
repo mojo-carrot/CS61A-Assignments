0028c5058b6f14de4d4e145800059811729d75cb
mojo-carrot <1781525229@qq.com>
2025-07-23 20:56:24 +0800

    hw05

--- HW/hw05/hw05.py
+++ HW/hw05/hw05.py
@@ -10,6 +10,14 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    yield n
+    if n != 1:
+        if n % 2 == 0:
+            yield from hailstone(n // 2)
+        else:
+            yield from hailstone(n * 3 + 1)
+    else:
+        yield from hailstone(1)
 
 
 def merge(a, b):
@@ -31,10 +39,16 @@ def merge(a, b):
     while True:
         if a_val == b_val:
             "*** YOUR CODE HERE ***"
+            yield a_val
+            a_val, b_val = next(a), next(b)
         elif a_val < b_val:
             "*** YOUR CODE HERE ***"
+            yield a_val
+            a_val = next(a)
         else:
             "*** YOUR CODE HERE ***"
+            yield b_val
+            b_val = next(b)
 
 
 def stair_ways(n):
@@ -51,6 +65,15 @@ def stair_ways(n):
     []
     """
     "*** YOUR CODE HERE ***"
+    if n == 0:
+        yield []
+    if n == 1:
+        yield [1]
+    if n > 1:
+        for p in stair_ways(n - 1):
+          yield [1] + p
+        for p in stair_ways(n - 2):
+          yield [2] + p
 
 
 def yield_paths(t, value):
@@ -89,10 +112,10 @@ def yield_paths(t, value):
     [[0, 2], [0, 2, 1, 2]]
     """
     if label(t) == value:
-        yield ____
+        yield [value]
     for b in branches(t):
-        for ____ in ____:
-            yield ____
+        for p in yield_paths(b, value):
+            yield [label(t)] + p
 
 
 
