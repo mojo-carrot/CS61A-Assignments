3ce93e1c6afdc8c607a23bf2bcfd5e81aa907833
mojo-carrot <1781525229@qq.com>
2025-08-21 10:42:03 +0800

    lab09

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,34 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (cond 
+    ((< num1 num2) -1)
+    ((> num1 num2) 1)
+    ((= num1 num2) 0)
+  )
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (define (inc x)
+    (+ num x)
+  )
+  inc
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+  (define (calc x)
+    (f (g x))
+  )
+  calc
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (define (repeat_f x)
+    (if (>= n 1)
+      ((repeat f (- n 1)) (f x))
+      x
+    )
+  )
+  repeat_f
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +40,9 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b)
+  (if (zero? b)
+    a
+    (gcd b (modulo a b))
+  )
+)
