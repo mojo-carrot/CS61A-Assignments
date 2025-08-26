141b589a3599466cf9e79f174ec5e2abaae05085
mojo-carrot <1781525229@qq.com>
2025-08-26 14:31:55 +0800

    hw08

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,35 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+    (if (or (null? s) (null? (cdr s)))
+        #t
+        (if (> (car s) (car (cdr s)))
+            #f
+            (ascending? (cdr s))))
+)
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (if (null? s)
+        nil
+        (if (pred (car s))
+            (cons(car s) (my-filter pred (cdr s)))
+            (my-filter pred (cdr s)))
+    )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (cond ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons(car lst1) (cons(car lst2) (interleave (cdr lst1) (cdr lst2)))))
+    )
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s) 
+    (define (not_equal_maker a)
+        (define (not_equal b)
+            (not (= a b))
+        )
+        not_equal
+    )
+    (if (null? s)
+        nil 
+        (cons (car s) (filter (not_equal_maker (car s)) (no-repeats (cdr s)))))
+)
